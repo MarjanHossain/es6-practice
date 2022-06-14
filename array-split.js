fbb47700f27f7e13985ed870ffc4d96e50dfbf1a
MarjanHossain <marjanhossain65@gmail.com>
2022-06-14 16:55:02 +0600

    js advaced

---
+++ array-split.js
@@ -0,0 +1,9 @@
+const nums =[1,2,3,4,5,6,7,8];
+// const part = nums.slice(2,5);
+// console.log(part);
+// console.log(nums);
+// const remove = nums.splice(3,6,44,55,66);
+// console.log(remove);
+// console.log(nums);
+const together = nums.join("আহো ভাতিজা আহো");
+console.log(together);
---
+++ closure.js
@@ -0,0 +1,10 @@
+function stopWatch() {
+    let count = 0;
+    return function () {
+        count ++;
+        return count;
+    }
+}
+const watch1 = stopWatch();
+console.log(watch1);
+console.log(watch1);
